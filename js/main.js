30faea39c8ac302c6fba6d08dc87ccd834523e30
Vladimir Himiak <vv.himiak@gmail.com>
2017-09-13 19:54:55 +0300

    commit-2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 $(function () {
 
-    "use strict";
+    $('.navbar-toggle').on('click', function() {
+        $('.navbar-collapse').slideToggle();
+    });
+
+    // Header Scroll
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 45) {
+            $('#navbar').css({
+                'padding-left' : '15px',
+                'padding-right' : '15px'
+            });
+            $('.header__content').css('margin-top', 235);
+            $('#navbar-fixed-top').addClass('navbar-fixed-top');
+            $('.header__top--wrap').addClass('fixed');
+        } else {
+            $('.header__content').css('margin-top', 207);
+            $('#navbar-fixed-top').removeClass('navbar-fixed-top');
+            $('.header__top--wrap').removeClass('fixed');
+        }
+    });
 
 });
+
+
